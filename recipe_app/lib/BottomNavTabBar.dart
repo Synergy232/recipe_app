9cbe3ba5f5b1173016d4895cccaeb9445cd80214
Synergy232 <warrent41@gmail.com>
2019-12-28 01:05:25 -0500

    added nav bar

---
+++ recipe_app/lib/BottomNavTabBar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class BottomTabbarExample extends StatefulWidget {
+  const BottomTabbarExample({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BottomTabbarExampleState();
+}
+
+class _BottomTabbarExampleState extends State<BottomTabbarExample>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  static const _kTabPages = <Widget>[
+    Center(child: Icon(Icons.cloud, size: 64.0, color: Colors.teal)),
+    Center(child: Icon(Icons.alarm, size: 64.0, color: Colors.cyan)),
+    Center(child: Icon(Icons.forum, size: 64.0, color: Colors.blue)),
+    Center(child: Icon(Icons.ac_unit, size: 64.0, color: Colors.green))
+    
+  ];
+  static const _kTabs = <Tab>[
+    Tab(icon: Icon(Icons.cloud), text: 'Breakfast'),
+    Tab(icon: Icon(Icons.alarm), text: 'Lunch'),
+    Tab(icon: Icon(Icons.forum), text: 'Dinner'),
+    Tab(icon: Icon(Icons.ac_unit), text: 'Dessert')
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _kTabPages.length,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: TabBarView(
+        children: _kTabPages,
+        controller: _tabController,
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.blue,
+        child: TabBar(
+          tabs: _kTabs,
+          controller: _tabController,
+        ),
+      ),
+    );
+  }
+}
--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'BottomNavTabBar.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -34,29 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
+    return BottomTabbarExample();
   }
 }
