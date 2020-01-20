7aee7a3133beb9ec09a70e39772574e6049d4dcf
Synergy232 <warrent41@gmail.com>
2020-01-20 12:03:28 -0500

    added some backgrounds and a listview

--- recipe_app/lib/Pages/Lunch.dart
+++ recipe_app/lib/Pages/Lunch.dart
@@ -3,7 +3,41 @@ import 'package:flutter/material.dart';
 class Lunch extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset('images/lunch.jpeg'));
+    // return Center(child: Image.asset('images/lunch.jpeg'));
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 200),
+          height: 50,
+      child: ListView(
+  scrollDirection: Axis.horizontal,
+  children: <Widget>[
+    Container(
+      width: 160.0,
+      color: Colors.red,
+      child: Text('Some Lunch lmao'),
+    ),
+    Container(
+      width: 160.0,
+      color: Colors.blueGrey,
+      child: Text('Some Lunch lmao'),
+    ),
+    Container(
+      width: 160.0,
+      color: Colors.red,
+      child: Text('Some Lunch lmao'),
+    ),
+    Container(
+      width: 160.0,
+      color: Colors.blueGrey,
+      child: Text('Some Lunch lmao'),
+    ),
+    Container(
+      width: 160.0,
+      color: Colors.red,
+      child: Text('Some Lunch lmao'),
+    ),
+  ],
+),
+    );
   }
 
 }
